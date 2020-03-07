c96055741fa84e24248ffee7547c78146fb02c47
Yongwoo, Lee <leeyw910205@gmail.com>
2020-03-07 13:58:59 +0900

    feat: category repository 추가

--- src/main/java/com/noto/spring/shopping/config/JpaConfig.java
+++ src/main/java/com/noto/spring/shopping/config/JpaConfig.java
@@ -1,9 +1,18 @@
 package com.noto.spring.shopping.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+
 @Configuration
-@EnableJpaAuditing //JPA Auditing 활성화
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider") //JPA Auditing 활성화
 public class JpaConfig {
+
+    @Bean("auditorProvider")
+    public AuditorAware<String> auditorPprovider() {
+        return () -> Optional.of("이영우");
+    }
 }
--- src/main/java/com/noto/spring/shopping/repository/CategoryRepository.java
+++ src/main/java/com/noto/spring/shopping/repository/CategoryRepository.java
@@ -1,4 +1,7 @@
 package com.noto.spring.shopping.repository;
 
-public class CategoryRepository {
+import com.noto.spring.shopping.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, String> {
 }
