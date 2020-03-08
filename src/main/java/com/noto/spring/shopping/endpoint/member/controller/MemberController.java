e441ca38f55c40b524ce526f7bcd386977715031
171179 <171179@ssg.com>
2020-03-08 20:30:31 +0900

    시큐리티 추가 (로그인페이지)

--- src/main/java/com/noto/spring/shopping/endpoint/member/controller/MemberController.java
+++ src/main/java/com/noto/spring/shopping/endpoint/member/controller/MemberController.java
@@ -1,12 +1,27 @@
 package com.noto.spring.shopping.endpoint.member.controller;
 
+import com.noto.spring.shopping.domain.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
   @RequestMapping("/loginForm")
   public String login() {
-    return "shopping/member/loginForm";
+    return "member/loginForm";
   }
+
+  @GetMapping("/register")
+  public String register(Model model) {
+    model.addAttribute("account", new Member());
+    return "member/loginForm";
+  }
+
+  @GetMapping("/finish")
+  public String finish() {
+    return "admin/account/finish";
+  }
+
 }
